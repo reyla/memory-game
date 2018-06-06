3240a75f417296db488106abb6662b8d1b0f80bc
Kate Speed <katespeed@gmail.com>
2018-06-05 20:25:33 -0400

    Add instructions and copyright

--- js/app.js
+++ js/app.js
@@ -252,3 +252,12 @@ $('.deck').on('click', '.card', function () {
 $('.restart').on('click', 'i', function () {
   reset();
 })
+
+
+/**
+* @description arrow icon takes user back to top of page
+*/
+$('.back-to-top').on('click', 'i', function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
